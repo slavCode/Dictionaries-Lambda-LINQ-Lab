f9ff6dc2e97591ffb5f5b024a19738e9fa3bcf40
slavCode <stomow@gmail.com>
2022-04-13 13:26:28 +0300

    Add project files.

---
+++ AssociativeArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssociativeArrays
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ');
+            var counts = new SortedDictionary<decimal, int>();
+
+            foreach (var num in numbers)
+            {
+                decimal parsedNum = decimal.Parse(num);
+                if (counts.ContainsKey(parsedNum)) counts[parsedNum]++;
+                else counts[parsedNum] = 1;
+            }
+
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}
