554be25a5937a94ce4914b7c2c47a75b8441a961
slavCode <stomow@gmail.com>
2022-04-13 14:00:40 +0300

    Add Odd Occurrences

---
+++ OddOccurrences/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddOccurrences
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().ToLower().Split();
+            var occurrencies = new Dictionary<string, int>();
+
+            foreach (var word in input)
+            {
+                if (occurrencies.ContainsKey(word)) occurrencies[word]++;
+                else occurrencies[word] = 1;
+            }
+
+            var result = occurrencies.Where(x => x.Value % 2 != 0)
+                                     .Select(x => x.Key)
+                                     .ToList();
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
